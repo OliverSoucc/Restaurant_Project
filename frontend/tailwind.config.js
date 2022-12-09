310ecd77ba24c0118f3698ee23360fa8a08f835e
OliverSoucc <oliv572v@easv365.dk>
2022-12-09 11:40:34 +0100

    static parts, hero, fetaures, cards, default form for user

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(assets/images/hero.jpg);",
+        'form-patter': "linear-gradient(to right bottom, #f3bf91, #e67e22);"
+      },
+    },
   },
   plugins: [],
 }
