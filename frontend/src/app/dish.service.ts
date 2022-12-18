c302b92a3ee4f2ad37c63bcd976be4b286807fdf
OliverSoucc <oliv572v@easv365.dk>
2022-12-18 14:49:10 +0100

    with deployed URL

--- frontend/src/app/dish.service.ts
+++ frontend/src/app/dish.service.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 
 
 export const customAxios = axios.create({
-  baseURL: 'http://localhost:5293',
+  baseURL: 'https://restaurant-project.azurewebsites.net/api',
 })
 @Injectable({
   providedIn: 'root'
@@ -13,45 +13,45 @@ export class DishService {
   constructor(private http: HttpClient) { }
 
   async getDishes(){
-    const dishes = await customAxios.get<any>('/api/dishes');
+    const dishes = await customAxios.get<any>('/dishes');
     return  dishes.data;
   }
 
   async getDish(dishId: number){
-    const dish = await customAxios.get<any>(`/api/dishes/${dishId}`);
+    const dish = await customAxios.get<any>(`/dishes/${dishId}`);
     return dish.data;
   }
 
   async createIngredient(ingredient: { amount: number; name: string }) {
-    await customAxios.post('/api/ingredients', ingredient);
+    await customAxios.post('/ingredients', ingredient);
   }
 
   async getIngredients(){
-    const ingredients = await customAxios.get('/api/ingredients');
+    const ingredients = await customAxios.get('/ingredients');
     return ingredients.data;
   }
 
   //change later
   async createDish(dish: { price: number; weekDay: string; name: string; description: string;}) {
-    const newDish = await customAxios.post('/api/dishes', dish);
+    const newDish = await customAxios.post('/dishes', dish);
     return newDish.data
   }
 
   async createDishIngredient(dishIngredient: {dishId: number, ingredientId: number}){
-    await customAxios.post('/api/dishingredient', dishIngredient);
+    await customAxios.post('/dishingredient', dishIngredient);
   }
 
   async deleteDish(id: number) {
-    const deleteDish = await customAxios.delete(`/api/dishes/${id}`);
+    const deleteDish = await customAxios.delete(`/dishes/${id}`);
     return deleteDish.data;
   }
 
   async updateDish(dish: {id:number ,price: number; weekDay: string; name: string; description: string;}){
-    const updatedDish = await customAxios.put('/api/dishes', dish)
+    const updatedDish = await customAxios.put('/dishes', dish)
     return updatedDish.data
   }
 
   async deleteDishIngredient(id: number){
-    const deleteDishIngredient = await customAxios.delete(`/api/ingredients/${id}`);
+    const deleteDishIngredient = await customAxios.delete(`/ingredients/${id}`);
   }
 }
